0ca24c9374b7993926a49535cfd030e93417c5ec
mayank jain <mayankjain1394@gmail.com>
2023-10-17 20:51:06 +0530

    Added toggleCompleted

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -5,6 +5,7 @@ contract TodoList {
     uint public taskCount;
     mapping(uint => Task) public tasks;
     event TaskCreated(uint id, string content, bool completed);
+    event TaskCompleted(uint id, bool completed);
 
     struct Task {
         uint id;
@@ -21,5 +22,12 @@ contract TodoList {
         tasks[taskCount] = Task(taskCount, _content, false);
 
         emit TaskCreated(taskCount, _content, false);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.completed = !_task.completed;
+        tasks[_id] = _task;
+        emit TaskCompleted(_id, _task.completed);
     }
 }
