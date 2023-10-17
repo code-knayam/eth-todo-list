c208f27d23915cfab1059080942ec5ec6eefe071
mayank jain <mayankjain1394@gmail.com>
2023-10-17 19:11:42 +0530

    Added list tasks UI

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,6 +3,20 @@ pragma solidity ^0.8.14;
 
 contract TodoList {
     uint public taskCount;
+    mapping(uint => Task) public tasks;
 
-    constructor() {}
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() {
+        createTask("Default Task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
